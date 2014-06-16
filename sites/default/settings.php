687fe88b2e6227940539466cd9c48ba04fc678a7
Ron Ferguson <ron.ferguson@reachlocal.com>
2014-06-16 13:39:16 -0500

    added nasdaq press release site credentials for API access

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -658,6 +658,11 @@ $conf['fast_404_HTML_error_all_paths'] = FALSE;
 # This setting will deliver 404s with less than 2MB of RAM.
 //fast_404_path_check();
 
+
+$conf['nasdaqomx_company_id'] = 'ABEA-4CSQ7X';
+$conf['nasdaqomx_pin'] = '887304226';
+$conf['nasdaqomx_domain'] = 'http://investors.reachlocal.com';
+
 /**
  * Create the local.settings.php file to override any variables set in this file
  * there for your local development environment.
